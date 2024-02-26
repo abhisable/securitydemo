34481200bb2fbbc292670f27602ff16b247ff556
Abhishek Sable <abhisheksable@Abhisheks-MacBook-Air.local>
2024-02-26 13:48:54 +0530

    getting started with spring security from basics, starting with maven project adding dependencies spring web security and spring security config

---
+++ src/main/java/com/securitydemo/config/MySecurityIntitializer.java
@@ -0,0 +1,7 @@
+package com.securitydemo.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class MySecurityIntitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
---
+++ src/main/java/com/securitydemo/controllers/HiController.java
@@ -0,0 +1,18 @@
+package com.securitydemo.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HiController {
+
+	@GetMapping("/hi")
+	public String sayHi(){
+		return "hi";
+	}
+	
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "hello";
+	}
+}
